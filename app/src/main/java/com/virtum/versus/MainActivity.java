3a12b6c50b730aafcbec0e6a9294446bad3f6cdb
Virtum <pawel.filocha@gmail.com>
2017-08-06 13:27:27 +0200

    Initial commit

---
+++ app/src/main/java/com/virtum/versus/MainActivity.java
@@ -0,0 +1,20 @@
+package com.virtum.versus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickCreate(View view) {
+        final TextView testView = findViewById(R.id.test_view);
+        testView.setText("Clicked!");
+    }
+}
