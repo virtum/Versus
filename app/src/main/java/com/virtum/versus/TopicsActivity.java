5e454782acbafd985fe08dc31ab142b8b5bc1cd8
Virtum <pawel.filocha@gmail.com>
2017-08-06 20:06:30 +0200

    Add basic flow of creating topic, and adding it to topic lists

--- app/src/main/java/com/virtum/versus/TopicsActivity.java
+++ app/src/main/java/com/virtum/versus/TopicsActivity.java
@@ -1,7 +1,10 @@
 package com.virtum.versus;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -21,12 +24,28 @@ public class TopicsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topics);
 
+
         lv = findViewById(R.id.list_options);
+        lv.setTextFilterEnabled(true);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+
+                Intent intent = new Intent(TopicsActivity.this,
+                        TopicDetailsActivity.class);
+                intent.putExtra("newTopic", topics.get(titles.get(position)));
+                startActivity(intent);
+            }
+        });
+
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 titles);
 
         lv.setAdapter(arrayAdapter);
+
+
     }
 }
